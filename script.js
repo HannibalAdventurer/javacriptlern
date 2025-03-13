c465fde0976a5cb5ad8abb9f303ddb9df91cf0e7
HannibalAdventurer <sternneue@gmail.com>
2025-03-13 06:29:08 -0400

    firstCode

---
+++ script.js
@@ -0,0 +1,19 @@
+let product = ["msi 12rtx", "lenovo 12rtx", "asus 12rtx"];
+
+product.push("Mac os");
+console.log(product);
+
+
+
+function Addpro() {
+    let nmb = document.getElementsByName("inc")[0];
+    nmb.value = parseInt(nmb.value) + 1;  //bich tbadel string lil integer
+    if(nmb )
+}
+
+
+
+function dec() {
+    let nmb = document.getElementsByName("inc")[0];
+    nmb.value = parseInt(nmb.value) - 1;  //bich tbadel string lil integer
+}
